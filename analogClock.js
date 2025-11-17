92ff80e45a284317a7c72c781b2bc72415e5643e
Latifa <latifa.ali.0210@gmail.com>
2025-11-17 16:25:07 +0100

    repo erstellt

--- analogClock.js
+++ analogClock.js
@@ -8,18 +8,19 @@ const secondHand = document.querySelector(".second-hand");
 function setClockHands() {
   const now = new Date();
 
+  // Get current seconds
   const seconds = now.getSeconds();
-
+  // Calculate degrees for seconds hand
   const secondsDegrees = (seconds / 60) * 360;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
+  // Get current minutes
   const minutes = now.getMinutes();
-
+  // Calculate degrees for minutes hand
   const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-
+  // Get current hours
   const hours = now.getHours() % 12;
-
+  // Calculate degrees for hours hand
   const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
